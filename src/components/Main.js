dfa16088eb2864827c9d849e6a247fda9fc15c42
ludoc0de <champi.ludo@tutanota.com>
2022-10-09 17:21:18 -0300

    create main and footer

---
+++ src/components/Main.js
@@ -0,0 +1,45 @@
+
+const Main = () => {
+  return (
+    <section>
+        <div>
+            {/* component 1*/}
+            <h2>Nos offres pour accompagner vos salariés</h2>
+            <h3>Offrez une expérience collaborateur unique centrée sur l'humain</h3>
+            <p>
+                Mademoiselle Happiness vous accompagne dans l'amélioration des conditions de travail et plus généralement le bonheur de vos salariés.
+                Nos experts vous proposent des méthodes axées sur le développement de l'individu et l'intelligence collective.
+                Nous vous aidons à la mise en œuvre d'un plan d'actions bien-être annuel sur mesure favorisant l’épanouissement durable de vos salariés pour gagner en performance individuelle et collective. 
+            </p>
+        </div>
+        <div>
+            {/* dont repeat add components for the 3 offers*/}
+            {/* <div>
+                <h4></h4>
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <h4></h4>
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <h4></h4>
+                <p></p>
+                <p></p>
+            </div> */}
+            <button>Demander un devis</button>
+            {/* component 1*/}
+            {/* component 1*/}
+            <button>Consultez le programme</button>
+            <button>Rencontrons-nous...</button>
+        </div>
+        <form>
+            
+        </form>
+    </section>
+  )
+}
+
+export default Main
